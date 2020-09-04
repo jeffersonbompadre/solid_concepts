e937bbd701f4d2a6ff70db78f427215c0be4a41e
jefferson.bompadre <jefferson.bompadre@smlbrasil.com.br>
2020-09-04 08:53:45 -0300

    Criando Data com 130K registros

--- CandiateTesting.JeffersonBompadre/CandidateTesting.JeffersonBompadre.AdjacentMaxDistance/Program.cs
+++ CandiateTesting.JeffersonBompadre/CandidateTesting.JeffersonBompadre.AdjacentMaxDistance/Program.cs
@@ -17,10 +17,10 @@ namespace CandidateTesting.JeffersonBompadre.AdjacentMaxDistance
             // para o cálculo do maior valo adjascente.
             if (args.Count() == 1)
                 MountDataArray(int.Parse(args[0]));
+
+            Console.WriteLine($"Iniciando o cálculo Maior vaor Adjacente {DateTime.Now}");
             AdjacentMaxDistanceFromBase();
             
-            //Usado apenas para teste de lógica montada para cálculo da distância adjacente
-            //AdjacentMaxDistance(MountArray(1000));
             Console.ReadKey();
         }
 
@@ -56,68 +56,5 @@ namespace CandidateTesting.JeffersonBompadre.AdjacentMaxDistance
             var elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:000}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
             Console.WriteLine($"Maior distância (DB): {maxValue}, Tempo: {elapsedTime}");
         }
-
-        #region Method Calc Adjacent Value From Array
-
-        /// <summary>
-        /// Monta um array local, para teste de lógica de cálculo distância adjacente (somente local)
-        /// </summary>
-        /// <param name="countElements"></param>
-        /// <returns></returns>
-        public static int[] MountArray(int countElements)
-        {
-            var random = new Random();
-            var adjacentArray = new int[countElements];
-            Parallel.For(0, countElements, i =>
-            {
-                adjacentArray[i] = random.Next(1, 40000);
-            });
-            return adjacentArray;
-            //return new int[] { 0, 3, 3, 7, 5, 3, 11, 1 };
-            //                   1  2  2  3  4  2  5   6
-        }
-
-        /// <summary>
-        /// Faz cálculo da maior distância adjacente de um array teste de lógica (somente local)
-        /// </summary>
-        /// <param name="adjacentArray"></param>
-        /// <returns></returns>
-        public static int AdjacentMaxDistance(int[] adjacentArray)
-        {
-            var stopWatch = new Stopwatch();
-            stopWatch.Start();
-            var max = 0;
-            Parallel.For(0, adjacentArray.Length - 1, x =>
-            {
-                Parallel.For(x + 1, adjacentArray.Length, y =>
-                {
-                    var aux = 0;
-                    var p = adjacentArray[x];
-                    var q = adjacentArray[y];
-                    if (p > q)
-                    {
-                        aux = p;
-                        p = q;
-                        q = aux;
-                    }
-                    var v = adjacentArray
-                        .Where(v => v > p && v < q)
-                        .Select(x => x)
-                        .ToList();
-                    if (v.Count == 0)
-                    {
-                        var distance = Math.Abs(p - q);
-                        max = Math.Max(max, distance);
-                    }
-                });
-            });
-            stopWatch.Stop();
-            var ts = stopWatch.Elapsed;
-            var elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:000}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
-            Console.WriteLine($"Maior distância: {max}, Tempo: {elapsedTime}");
-            return max;
-        }
-
-        #endregion
     }
 }
